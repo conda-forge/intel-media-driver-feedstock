15700a23747f4366f4358416e6cf919ab8b9f7d9
Mark Harfouche <mark.harfouche@gmail.com>
2025-03-03 09:56:05 -0500

    fix LIBVA_DRIVERS_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,13 @@ set -ex
 mkdir build
 pushd build
 
-cmake ${CMAKE_ARGS} ..
+# LIBVA_DRIVERS_PATH must be specified so that things are found correctly
+# in our splayed environments in conda forge
+# However, the environment variable conflicts with the name
+# of the install path
+cmake ${CMAKE_ARGS} \
+   -DLIBVA_DRIVERS_PATH=${PREFIX}/lib/dri \
+   ..
 
 make -j${CPU_COUNT}
 
